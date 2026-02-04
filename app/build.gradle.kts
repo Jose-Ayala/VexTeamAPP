a8727082997e10b20d27be78f81f3b7165b9ef52
Jose Ayala <jayala4145@gmail.com>
2026-02-04 15:00:55 -0500

    Updated version code and name.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.jayala.vexapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         buildConfigField("String", "ROBOT_EVENTS_TOKEN", "\"$robotEventsToken\"")
 
